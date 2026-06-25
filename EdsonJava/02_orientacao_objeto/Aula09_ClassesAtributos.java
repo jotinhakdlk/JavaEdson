c81d5dea81cc612075bed668a8a98f3bb9e6ac2f
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-06-25 09:04:57 -0300

    Add files via upload

---
+++ EdsonJava/02_orientacao_objeto/Aula09_ClassesAtributos.java
@@ -0,0 +1,13 @@
+package 02_orientacao_objeto;
+
+public class Aula09_ClassesAtributos {
+    Carro09 meuCarro = new Carro09();
+    meuCarro.modelo = "Fusca";
+    meuCarro.cor = "azul";
+    meuCarro.ano = 1975;
+
+    Carro09.meuCarro2 = new Carro09();
+    meuCarro2.modelo = "Fiat";
+
+    system.out.println("")
+}
