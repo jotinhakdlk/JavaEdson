a16eb6b7d53908455ef1db6881eb4e956a0f187a
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-06-26 08:23:10 -0300

    Add files via upload

---
+++ EdsonJava/02_orientacao_objeto/Aula09B_ClassesScanner.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+
+public class Aula09B_ClassesScanner {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(system.in);
+        Carro09 meuCarro = new Carro09();
+
+        System.out.println("==Cadastro de Veículo==");
+        System.out.println("Digite o modelo do carro: ")
+        meuCarro.modelo = teclado.nextLine();
+
+        System.out.println("Digite a cor do carro: ");
+        meuCarro.cor = teclado.nextLine();
+
+        System.out.println("Digite o ano do carro: ");
+        meuCarro.ano = teclado.nextLine();
+
+        System.out
+    }
+}
